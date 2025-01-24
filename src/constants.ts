6e5832ad3419a0fb5dd01d951fc51cb635a5e4df
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-01-24 23:51:22 +0200

    Change config

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const SITE_NAME = "SaaS Template"
+export const SITE_NAME = "Test SaaS Template"
 export const SITE_DESCRIPTION = "A modern SaaS template built with Next.js 14 and Cloudflare Workers, designed for scalability and performance."
-export const SITE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://saas-stack.startupstudio.dev"
+export const SITE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://test-saas-stack.startupstudio.dev"
 export const GITHUB_REPO_URL = "https://github.com/LubomirGeorgiev/cloudflare-workers-nextjs-saas-template"
 
 export const SITE_DOMAIN = new URL(SITE_URL).hostname
